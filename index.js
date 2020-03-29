e697c3ad3cc7e3b0767775bd826e98044e89fceb
Nathan <Nathan@192.168.0.133>
2020-03-29 16:16:53 -0700

    add home route for uptime check

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const tmi = require('tmi.js');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.sendStatus(200));
+
 app.listen(port, () => {
    console.log(`listening on ${port}`);
 });
